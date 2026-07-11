d3f0edfaa6b1f69fa936a0e808fc8ead6d386e56
MaksLew <lewandowski1maksymilian@gmail.com>
2026-07-11 21:05:25 +0200

    [nvim] change some custom highlights

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -4,6 +4,41 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
+		local function apply_theme()
+			local scheme = vim.fn.system({ "gsettings", "get", "org.gnome.desktop.interface", "color-scheme" })
+			vim.o.background = scheme:find("prefer-dark", 1, true) and "dark" or "light"
+			vim.cmd.colorscheme(vim.o.background == "dark" and "catppuccin" or "iceberg")
+			if vim.o.background == "light" then
+				vim.api.nvim_set_hl(0, "Normal", { fg = "#33374c", bg = "NONE" })
+				vim.api.nvim_set_hl(0, "StatusLine", { fg = "#33374c", bg = "NONE" })
+				vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "#8389a3", bg = "NONE" })
+				for _, name in ipairs({
+					"CursorLine",
+					"CursorLineNr",
+					"CursorLineSign",
+					"CursorLineFold",
+					"LineNr",
+					"LineNrAbove",
+					"LineNrBelow",
+					"SignColumn",
+					"FoldColumn",
+					"DiagnosticSignError",
+					"DiagnosticSignWarn",
+					"DiagnosticSignInfo",
+					"DiagnosticSignHint",
+					"DiagnosticSignOk",
+					"GitGutterAdd",
+					"GitGutterChange",
+					"GitGutterChangeDelete",
+					"GitGutterDelete",
+				}) do
+					local hl = vim.api.nvim_get_hl(0, { name = name, link = false })
+					hl.bg = nil
+					vim.api.nvim_set_hl(0, name, hl)
+				end
+			end
+		end
+
 		require("catppuccin").setup({
 			flavour = "mocha",
 			transparent_background = true,
@@ -12,8 +47,9 @@ return {
 			no_underline = true,
 			custom_highlights = function()
 				return {
-					CursorLineNr = { fg = "#f9e2af" },
-					LineNr = { fg = "#a6adc8" },
+					CursorLineNr = { fg = colors.blue },
+					CursorLine = { bg = "NONE" },
+					LineNr = { fg = colors.overlay1 },
 				}
 			end,
 		})
